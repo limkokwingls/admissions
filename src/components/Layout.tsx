8d33718d32a15429928c7201fb739788f02b882c
Ntholi Nkhatho <ntholi@live.com>
2023-05-25 19:06:33 +0200

    Add home Link

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,18 +1,25 @@
-import { Title, Stack, Image, Center, Paper } from '@mantine/core';
+import { Title, Stack, Image, Center, Paper, Button } from '@mantine/core';
+import Link from 'next/link';
 import React from 'react';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }: any) {
+  const router = useRouter();
+  const isHome = router.pathname === '/';
   return (
     <>
-      <Stack mb='xl'>
-        <Title mt='xl' ta='center'>
-          Admission List
-        </Title>
-        <Center>
-          <Paper withBorder p='sm'>
-            <Image src='/logo.png' width='250px' alt='logo' />
-          </Paper>
-        </Center>
+      <Title mt='xl' ta='center'>
+        Admission List
+      </Title>
+      <Stack mb='xl' align='center' p='sm'>
+        {!isHome && (
+          <Link href='/'>
+            <Button color='dark'>Home</Button>
+          </Link>
+        )}
+        <Paper withBorder p='sm'>
+          <Image src='/logo.png' width='250px' alt='logo' />
+        </Paper>
       </Stack>
       {children}
     </>
