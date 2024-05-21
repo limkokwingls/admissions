76424b7e1fa8bbb1f5d5477fbacf565c9ee64f49
Ntholi Nkhatho <ntholi@live.com>
2024-05-21 12:04:27 +0200

    refactor: Replace program name div with Button component in program level page

--- src/app/programs/[level]/[program]/page.tsx
+++ src/app/programs/[level]/[program]/page.tsx
@@ -2,7 +2,7 @@ import { db } from '@/lib/firebase';
 import { collection, getDocs, query, where } from 'firebase/firestore';
 import React from 'react';
 import StudentsTable from './StudentsTable';
-import { Button, Link } from '@nextui-org/react';
+import { Button, Card, CardFooter, Link } from '@nextui-org/react';
 import { MdArrowBack } from 'react-icons/md';
 
 type Props = {
@@ -27,12 +27,14 @@ export default async function Programs({ params: { program } }: Props) {
 
   return (
     <main>
-      <header className='flex gap-20 sm:gap-0 items-center justify-between'>
-        <Button isIconOnly variant='bordered' as={Link} href='.'>
-          <MdArrowBack />
-        </Button>
-        <h1 className='text-sm'>{program}</h1>
-      </header>
+      <Card>
+        <CardFooter className='flex gap-20 sm:gap-0 items-center justify-between'>
+          <Button isIconOnly variant='bordered' as={Link} href='.'>
+            <MdArrowBack />
+          </Button>
+          <h1 className='text-sm'>{program}</h1>
+        </CardFooter>
+      </Card>
       <section className='mt-10'>
         <StudentsTable students={students} />
       </section>
