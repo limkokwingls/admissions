df12e1d1fcd13878080d5c6ce8391aacc009d2ee
Ntholi Nkhatho <ntholi@live.com>
2024-05-21 08:15:51 +0200

    feat: Add student name lookup functionality to home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,10 @@ export default function RootLayout({
           <header className='flex flex-col items-center'>
             <Link href='/'>
               <Image alt='Logo' src='/logo.png' width={280} height={125} />
+              <h1 className='text-center text-xl'>Student Admissions</h1>
             </Link>
           </header>
-          {children}
+          <div className='mt-10'>{children}</div>
         </Providers>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import { Button } from '@nextui-org/react';
+import { Button, Input } from '@nextui-org/react';
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main>
-      <header></header>
+    <main className='flex flex-col items-center'>
+      <div className='flex gap-3'>
+        <Input
+          type='text'
+          placeholder='Your names'
+          variant='bordered'
+          className='sm:w-72'
+        />
+        <Button color='primary'>Lookup</Button>
+      </div>
     </main>
   );
 }
