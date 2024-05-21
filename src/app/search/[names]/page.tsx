00d775683ba4aa237ea2d963288d110bc913923d
Ntholi Nkhatho <ntholi@live.com>
2024-05-21 12:44:14 +0200

    refactor: Improve student search functionality in search page

--- src/app/search/[names]/page.tsx
+++ src/app/search/[names]/page.tsx
@@ -1,5 +1,12 @@
 import { db } from '@/lib/firebase';
-import { collection, getDocs, query, where, or } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  QueryDocumentSnapshot,
+} from 'firebase/firestore';
 import React from 'react';
 import StudentsTable from './StudentsTable';
 import { Button, Card, CardFooter, Link } from '@nextui-org/react';
@@ -11,22 +18,39 @@ type Props = {
   };
 };
 
+// student in the database have firstName and lastName, but we are passing names which
+// might be the firstName or lastName or both, so we need to search for students with
+// either the firstName or lastName or both matching the names
 async function getStudents(fullName: string) {
-  const names = fullName.trim().toUpperCase();
-  const students: Student[] = [];
+  const names = fullName.toUpperCase().trim().split(/\s+/);
+  const firstName = names[0];
+  const lastName = names.length > 1 ? names[names.length - 1] : '';
+
+  const studentsCollection = collection(db, 'students');
+  const queryConstraints = [];
+
+  if (firstName) {
+    queryConstraints.push(where('names', '==', firstName));
+  }
+
+  if (lastName) {
+    queryConstraints.push(where('surname', '==', lastName));
+  }
+
   const q = query(
-    collection(db, 'students'),
-    or(
-      where('firstName', '==', names),
-      where('lastName', '==', names),
-      where('firstName', '==', names.split(' ')[0]),
-      where('lastName', '==', names.split(' ')[1])
-    )
+    studentsCollection,
+    ...queryConstraints,
+    orderBy('names'),
+    orderBy('surname')
   );
+
   const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
+  const students: Student[] = [];
+
+  querySnapshot.forEach((doc: QueryDocumentSnapshot) => {
     students.push(doc.data() as Student);
   });
+
   return students;
 }
 
@@ -38,7 +62,7 @@ export default async function Programs({ params: { names } }: Props) {
     <main>
       <Card>
         <CardFooter className='flex gap-20 sm:gap-0 items-center justify-between'>
-          <Button isIconOnly variant='bordered' as={Link} href='.'>
+          <Button isIconOnly variant='bordered' as={Link} href='..'>
             <MdArrowBack />
           </Button>
           <h1 className='text-sm'>{names}</h1>
