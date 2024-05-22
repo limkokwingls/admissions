51018495222b393b750e13dfeb1bc0bdd867ceca
Ntholi Nkhatho <ntholi@live.com>
2024-05-22 21:10:28 +0200

    refactor: Add Card component with acceptance letter information

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@nextui-org/react';
+import { Button, Card, CardBody, CardHeader } from '@nextui-org/react';
 import Link from 'next/link';
 import { MdArrowRight } from 'react-icons/md';
 import LookupField from './home/LookupField';
@@ -27,6 +27,17 @@ export default function Home() {
           Diploma Programs
         </Button>
       </section>
+
+      <Card className='mt-10 sm:w-[33rem] sm:mx-auto bg-default/30'>
+        <CardBody className='p-6 sm:p-8'>
+          <p className='text-sm'>
+            Admitted students must collect their acceptance letters from the
+            Limkokwing Registry Department and submit the completed forms by
+            <span className='font-bold'>June 7, 2024</span>. Failure to do so
+            will result in losing your slot.
+          </p>
+        </CardBody>
+      </Card>
     </main>
   );
 }
