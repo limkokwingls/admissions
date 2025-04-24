c07e24c0edf50b94b51a5e0bf7d3863cac2d9662
Ntholi Nkhatho <ntholi@live.com>
2025-04-24 13:26:16 +0200

    init NextJs project

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Limkokwing Admissions',
+  description: '2025/2026 Fist Year Limkokwing Admissions',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
