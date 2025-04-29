f7015f9545e5cd7e4ea118eb2d2395f5f437d1d2
Ntholi Nkhatho <ntholi@live.com>
2025-04-29 16:53:34 +0200

    feat: Add student repository with search and pagination functionality

--- src/server/students/repository.ts
+++ src/server/students/repository.ts
@@ -1,5 +1,7 @@
-import BaseRepository from '@/server/base/BaseRepository';
-import { students } from '@/db/schema'
+import BaseRepository, { QueryOptions } from '@/server/base/BaseRepository';
+import { students } from '@/db/schema';
+import { db } from '@/db';
+import { and, eq, like } from 'drizzle-orm';
 
 export default class StudentRepository extends BaseRepository<
   typeof students,
@@ -7,6 +9,18 @@ export default class StudentRepository extends BaseRepository<
 > {
   constructor() {
     super(students, 'id');
+  }
+
+  override async query(params: QueryOptions<typeof students>) {
+    const criteria = this.buildQueryCriteria({
+      ...params,
+      filter: and(like(students.surname, `%${params.search}%`)),
+    });
+    const data = await db.query.students.findMany({
+      ...criteria,
+    });
+
+    return this.createPaginatedResult(data, criteria);
   }
 }
 
