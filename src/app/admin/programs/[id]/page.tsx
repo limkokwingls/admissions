71e76a36bf3ca911b3bf2ed2c8ef4a55dbee69df
Ntholi Nkhatho <ntholi@live.com>
2025-04-30 19:53:31 +0200

    Add CRUD functionality for academic programs management

---
+++ src/app/admin/programs/[id]/page.tsx
@@ -0,0 +1,39 @@
+import {
+  DetailsView,
+  DetailsViewHeader,
+  FieldView,
+  DetailsViewBody,
+} from '@/components/adease';
+import { notFound } from 'next/navigation';
+import { getProgram, deleteProgram } from '@/server/programs/actions';
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+export default async function ProgramDetails({ params }: Props) {
+  const { id } = await params;
+  const program = await getProgram(Number(id));
+  
+  if (!program) {
+    return notFound();
+  }
+
+  return (
+    <DetailsView>
+      <DetailsViewHeader 
+        title={'Program'} 
+        queryKey={['programs']}
+        handleDelete={async () => {
+          'use server';
+          await deleteProgram(Number(id));
+        }}
+      />
+      <DetailsViewBody>
+        <FieldView label='Id'>{program.id}</FieldView>
+        <FieldView label='Code'>{program.code}</FieldView>
+        <FieldView label='Name'>{program.name}</FieldView>
+      </DetailsViewBody>
+    </DetailsView>
+  );
+}
