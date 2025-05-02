05df8ae9be4b93536a296ccab49727fc0760d507
Ntholi Nkhatho <ntholi@live.com>
2025-05-02 14:16:02 +0200

    Add properties schema and export database schemas

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,4 @@
 export * from './auth';
 export * from './students';
 export * from './programs';
+export * from './properties';
---
+++ src/db/schema/properties.ts
@@ -0,0 +1,9 @@
+import { integer, sqliteTable, text, real } from 'drizzle-orm/sqlite-core';
+
+export const properties = sqliteTable('properties', {
+  id: text().default('2025').primaryKey(),
+  acceptanceFee: real().notNull(),
+  acceptanceDeadline: integer({ mode: 'timestamp' }).notNull(),
+  registrationDate: integer({ mode: 'timestamp' }).notNull(),
+  orientationDate: integer({ mode: 'timestamp' }).notNull(),
+});
