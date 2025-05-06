c532f06c7a427646569506b0f86f5ed3a110f70a
Ntholi Nkhatho <ntholi@live.com>
2025-05-06 15:44:59 +0200

    update student list layout with accepted status indicator and new student link

--- src/app/admin/students/layout.tsx
+++ src/app/admin/students/layout.tsx
@@ -3,6 +3,7 @@
 import { PropsWithChildren } from 'react';
 import { ListItem, ListLayout, NewLink } from '@/components/adease';
 import { getStudents } from '@/server/students/actions';
+import { IconCheck } from '@tabler/icons-react';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
@@ -12,7 +13,11 @@ export default function Layout({ children }: PropsWithChildren) {
       getData={getStudents}
       actionIcons={[<NewLink key={'new-link'} href='/admin/students/new' />]}
       renderItem={(it) => (
-        <ListItem id={it.id} label={`${it.surname} ${it.names}`} />
+        <ListItem
+          id={it.id}
+          label={`${it.surname} ${it.names}`}
+          rightSection={it.accepted ? <IconCheck size={'1rem'} /> : undefined}
+        />
       )}
     >
       {children}
