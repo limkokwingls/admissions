182158e8afc3964aebc4b5cc5c41e6411f4ea1e3
Ntholi Nkhatho <ntholi@live.com>
2025-05-09 16:04:29 +0200

    feat: add layout component for calls admin page with status filtering

--- src/app/admin/calls/[status]/[id]/edit/page.tsx
+++ src/app/admin/calls/[status]/[id]/edit/page.tsx
@@ -2,13 +2,14 @@ import { Box } from '@mantine/core';
 import { notFound } from 'next/navigation';
 import Form from '../../Form';
 import { getCall, updateCall } from '@/server/calls/actions';
+import { CallStatus } from '@/db/schema';
 
 type Props = {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: string; status: string }>;
 };
 
 export default async function CallEdit({ params }: Props) {
-  const { id } = await params;
+  const { id, status } = await params;
   const call = await getCall(id);
   if (!call) {
     return notFound();
@@ -23,6 +24,7 @@ export default async function CallEdit({ params }: Props) {
           'use server';
           return await updateCall(id, value);
         }}
+        status={status as CallStatus}
       />
     </Box>
   );
--- src/app/admin/calls/[status]/layout.tsx
+++ src/app/admin/calls/[status]/layout.tsx
@@ -3,20 +3,18 @@
 import { ListItem, ListLayout, NewLink } from '@/components/adease';
 import { CallStatus } from '@/db/schema';
 import { getCalls } from '@/server/calls/actions';
+import { useParams } from 'next/navigation';
 
 type Props = {
-  params: {
-    status: string;
-  };
   children: React.ReactNode;
 };
 
-export default function Layout({ children, params }: Props) {
-  const { status } = params;
+export default function Layout({ children }: Props) {
+  const { status } = useParams();
   return (
     <ListLayout
       path={'/admin/calls'}
-      queryKey={['calls', status]}
+      queryKey={['calls', status as string]}
       getData={(page, search) => getCalls(status as CallStatus, page, search)}
       actionIcons={[
         <NewLink key={'new-link'} href={`/admin/calls/${status}/new`} />,
