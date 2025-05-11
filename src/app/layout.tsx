7875b9f3a5d5b6884586cd0e45abc4a2df969089
Ntholi Nkhatho <ntholi@live.com>
2025-05-11 15:42:21 +0200

    update root layout with metadata for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,18 +4,33 @@ import Providers from './providers';
 export const metadata: Metadata = {
   title: 'Limkokwing Admissions',
   description:
-    '2025/2026 Fist Year Admissions, Limkokwing University of Creative Technology Lesotho',
+    '2025/2026 First Year Admissions, Limkokwing University of Creative Technology Lesotho',
   icons: {
     icon: '/images/logo.png',
   },
   openGraph: {
+    title: 'Limkokwing University Admissions 2025/2026',
+    description:
+      'Official list of 2025/2026 First Year Admitted Students at Limkokwing University of Creative Technology Lesotho',
+    url: 'https://luctintake.vercel.app',
+    siteName: 'Limkokwing University Lesotho',
+    locale: 'en_US',
+    type: 'website',
     images: [
       {
         url: '/images/logo.png',
-        width: 1371,
-        height: 691,
+        width: 1200,
+        height: 630,
+        alt: 'Limkokwing University Admissions',
       },
     ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Limkokwing University Admissions 2025/2026',
+    description:
+      'Official list of 2025/2026 First Year Admitted Students at Limkokwing University of Creative Technology Lesotho',
+    images: ['/images/logo.png'],
   },
 };
 
@@ -25,13 +40,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <html lang='en' suppressHydrationWarning>
-        <head />
-        <body>
-          <Providers>{children}</Providers>
-        </body>
-      </html>
+    <html lang='en' suppressHydrationWarning>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
